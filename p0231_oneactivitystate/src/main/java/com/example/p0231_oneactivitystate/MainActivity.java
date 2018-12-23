2382fdce3bbe45fda385b609ead98b6dbf28e04f
TarasovVP <vptarasov1998@gmail.com>
2018-12-23 21:09:24 +0200

    Initial commit

---
+++ p0231_oneactivitystate/src/main/java/com/example/p0231_oneactivitystate/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.p0231_oneactivitystate;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button btnActTwo;
+
+    private final String TAG = "Lifecycle";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+
+        Log.d(TAG, "MainActivity: onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "MainActivity: onStart()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "MainActivity: onRestart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "MainActivity: onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "MainActivity: onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "MainActivity: onDestroy()");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnActTwo:
+                Intent intent = new Intent(this, ActivityTwo.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+}
---
+++ p0321_simplebrowser/src/main/java/com/example/p0321_simplebrowser/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.p0321_simplebrowser;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        (findViewById(R.id.btnWeb)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://startandroid.ru/ru/uroki/vse-uroki-spiskom/72-urok-32-pishem-prostoj-brauzer.html")));
+            }
+        });
+
+    }
+}
